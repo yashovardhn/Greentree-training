390a9ac9f9cf0bfad14b749227c159c7738bed47
Yashovardhn <yashovardhn@gmail.com>
2025-07-09 18:47:59 +0530

    gitignore

--- Rest_Api/rest_practise/users/serializers.py
+++ Rest_Api/rest_practise/users/serializers.py
@@ -7,13 +7,28 @@ class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(read_only=True)
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        password = validated_data.pop('password')
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
 
         
         return user
+    
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop('password')
+            old_password = validated_data.pop('old_password')
+            if user.check_password(old_password):
+                user.set_password(password)
+                user.save()
+            else:
+                raise serializers.ValidationError("Old password is incorrect.")
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError({"info": err})
+        return super(UserSerializer, self).update(instance, validated_data)
 
     class Meta:
         model = User
