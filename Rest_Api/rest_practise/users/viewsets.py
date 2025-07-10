8bf84303b21d0f592bca01e241d66180b8a42f5e
Yashovardhn <yashovardhn@gmail.com>
2025-07-10 13:15:21 +0530

    profile creation

--- Rest_Api/rest_practise/users/viewsets.py
+++ Rest_Api/rest_practise/users/viewsets.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer  
+from .serializers import UserSerializer, ProfileSerializer 
 from .permissions import IsUserOwnerOrGetAndPostOnly  
+from .models import Profile  
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -9,7 +10,13 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     permission_classes = [IsUserOwnerOrGetAndPostOnly] 
     queryset = User.objects.all()
-    serializer_class = UserSerializer # Adjust the import path as necessary
+    serializer_class = UserSerializer 
     # Use the custom permission class
 
-    
+class ProfileViewSet(viewsets.ModelViewSet):
+    # """
+    # A viewset for viewing and editing user profile instances.
+    # """
+    # permission_classes = [IsUserOwnerOrGetAndPostOnly] 
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
