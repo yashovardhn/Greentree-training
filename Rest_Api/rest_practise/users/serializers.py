2f21e5eb7c1d5ba24fa94bb6e1ebaacff525d9ed
Yashovardhn <yashovardhn@gmail.com>
2025-07-10 12:57:10 +0530

    USCV

--- Rest_Api/rest_practise/users/serializers.py
+++ Rest_Api/rest_practise/users/serializers.py
@@ -4,7 +4,22 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        request_method = self.context['request'].method
+        password = data.get('password', None)
+        if request_method == 'POST':
+            if password == None:
+                raise serializers.ValidationError({"info":"Please provide a password."})
+        elif request_method == 'PUT' or request_method == 'PATCH':
+            old_password = data.get('old_password', None)
+            if password != None and old_password == None:
+                raise serializers.ValidationError({"info":"Please provide the old password."})
+        return data
+
+
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -18,18 +33,19 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         try:
             user = instance
-            password = validated_data.pop('password')
-            old_password = validated_data.pop('old_password')
-            if user.check_password(old_password):
-                user.set_password(password)
+            if 'password' in validated_data:
+                password = validated_data.pop('password')
+                old_password = validated_data.pop('old_password')
+                if user.check_password(old_password):
+                    user.set_password(password)
+                    user.save()
+                else:
+                    raise serializers.ValidationError("Old password is incorrect.")
                 user.save()
-            else:
-                raise serializers.ValidationError("Old password is incorrect.")
-            user.save()
         except Exception as err:
             raise serializers.ValidationError({"info": err})
         return super(UserSerializer, self).update(instance, validated_data)
 
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password', 'old_password']
